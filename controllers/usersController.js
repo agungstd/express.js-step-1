99840fee29e8fa6178b8bb68496a2e988412ea9d
agungstd <apakatadunia11@gmail.com>
2025-03-13 08:03:05 +0700

    implement proper async/await pattern

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,180 +1,136 @@
 const { v4: uuidv4 } = require("uuid");
-
 const Users = require("../models/Users");
 
-// let users = [
-//     {id: 1, name: 'Daban', email:'daban@email.com'},
-//     {id: 2, name: 'Yan ', email:'yan@email.com'},
-// ]
-
 module.exports = {
-  index: (req, res) => {
-    // // Di laravel seperti metode all()
-    // Users.find({}, "name _id", function(err, users){
-    //     if(err) console.log(err)
-
-    //     console.log(users)
-    //     res.render('pages/users/index', {users})
-    // })
-
-    // // Di laravel seperti menggunakan where email ...
-    // Users.find({name: 'Baliho'}, function(err, users){
-    //     if(err) console.log(err)
-
-    //     res.render('pages/users/index', {users})
-    // })
-
-    // // Di laravel seperti menggunakan first() ...
-    // Users.findOne({name: 'Baliho'}, function(err, users){
-    //     if(err) console.log(err)
-
-    // // Di laravel seperti query LIKE biasa digunakan di search
-    let search = {};
-
-    if (req.query.search) {
-      search = { name: { $regex: req.query.search } }; //contoh hasil = /search/
-    }
-    // cara 1
-    // Users.find(search, "name _id", function(err, users){
-    //     if(err) console.log(err)
-
-    //     console.log(users)
-    //     res.render('pages/users/index', {users})
-    // })
-
-    // cara 2
-    const query = Users.find(search);
-    query.select("name _id");
-    query.exec(function (err, users) {
-      if (err) console.log(err);
-
-      console.log(users);
+  // Display a listing of users with optional search functionality
+  index: async (req, res) => {
+    try {
+      // Create search query if search parameter exists
+      const searchQuery = req.query.search 
+        ? { name: { $regex: req.query.search, $options: 'i' } } 
+        : {};
+      
+      // Find users matching search criteria
+      const users = await Users.find(searchQuery).select("name _id");
+      
       res.render("pages/users/index", { users });
-    });
-
-    //     console.log(users)
-    //     res.render('pages/users/index', {users})
-    // })
-
-    // dibawah ini diambil dari data json diatas let users
-    // res.render('pages/users/index', {users})
+    } catch (error) {
+      console.error("Error fetching users:", error);
+      res.status(500).render("pages/error", { error: "Failed to fetch users" });
+    }
   },
 
+  // Show form to create a new user
   create: (req, res) => {
     res.render("pages/users/create");
   },
 
-  store: (req, res) => {
-    // MongoDB cara 1
-    // const user = new Users ({
-    //     id: uuidv4(),
-    //     name: req.body.name,
-    //     email: req.body.email,
-    //     password: req.body.password,
-    // })
-    // user.save(function(err, data) {
-    //     if (err) console.log(err)
-    //     // saved!
-    //     console.log(data)
-    //     res.redirect('/users')
-    // })
-
-    // MongoDB cara 2
-    Users.create(
-      {
+  // Store a newly created user
+  store: async (req, res) => {
+    try {
+      const { name, email, password } = req.body;
+      
+      await Users.create({
         id: uuidv4(),
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-      },
-      function (err, data) {
-        if (err) console.log(err);
-        // saved!
-        console.log(data);
-        res.redirect("/users");
-      }
-    );
-
-    // Test Json
-    // users.push({
-    //     id: uuidv4(),
-    //     name: req.body.name,
-    //     email: req.body.email,
-    // })
-    // console.log(users)
-    // res.end()
-  },
-
-  show: (req, res) => {
-    const id = req.params.id;
-    Users.findById(id, function (err, user) {
-      if (err) console.log(err);
-
-      console.log(user);
-      res.render("pages/users/detail", { user });
-    });
-
-    // Test Json
-    // const data = users.filter(user => {
-    //     return user.id == id
-    //     })
-    // // res.send(data)
-    // // console.log(id);
-    // res.render('pages/users/detail', {user: data})
-  },
-
-  edit: (req, res) => {
-    const id = req.params.id;
-    Users.findById(id, function (err, user) {
-      if (err) console.log(err);
-
-      console.log(user);
-      res.render("pages/users/edit", { user });
-    });
-  },
-
-  update: (req, res) => {
-    const id = req.params.id;
-
-    Users.findById(id, function (err, user) {
-      if (err) console.log(err);
-
-      if (user.id == id) {
-        user.name = req.body.name;
-        user.email = req.body.email;
-      }
-      console.log(user);
+        name,
+        email,
+        password, // Note: In production, this should be hashed
+      });
+      
       res.redirect("/users");
-    });
-
-    // TEST JSON
-    // users.filter(user => {
-    //     if(user.id == id){
-    //         user.id = id
-    //         user.name = req.body.name
-    //         user.email = req.body.email
-
-    //         return user
-    //     }
-    //     })
-    //     res.json({
-    //         status: true,
-    //         data: users,
-    //         message: 'Data user updated successfully!',
-    //         method: req.method,
-    //         url: req.url
-    // })
+    } catch (error) {
+      console.error("Error creating user:", error);
+      res.status(500).render("pages/users/create", { 
+        error: "Failed to create user",
+        formData: req.body 
+      });
+    }
   },
 
-  destroy: (req, res) => {
-    let id = req.params.userId;
-    users = users.filter((user) => user.id != id);
-    res.json({
-      status: true,
-      data: users,
-      message: "Data user deleted successfully!",
-      method: req.method,
-      url: req.url,
-    });
+  // Display a specific user
+  show: async (req, res) => {
+    try {
+      const user = await Users.findById(req.params.id);
+      
+      if (!user) {
+        return res.status(404).render("pages/error", { error: "User not found" });
+      }
+      
+      res.render("pages/users/detail", { user });
+    } catch (error) {
+      console.error("Error fetching user details:", error);
+      res.status(500).render("pages/error", { error: "Failed to fetch user details" });
+    }
   },
+
+  // Show form to edit user
+  edit: async (req, res) => {
+    try {
+      const user = await Users.findById(req.params.id);
+      
+      if (!user) {
+        return res.status(404).render("pages/error", { error: "User not found" });
+      }
+      
+      res.render("pages/users/edit", { user });
+    } catch (error) {
+      console.error("Error fetching user for edit:", error);
+      res.status(500).render("pages/error", { error: "Failed to fetch user" });
+    }
+  },
+
+  // Update the specified user
+  update: async (req, res) => {
+    try {
+      const { name, email } = req.body;
+      
+      const user = await Users.findByIdAndUpdate(
+        req.params.id, 
+        { name, email },
+        { new: true }
+      );
+      
+      if (!user) {
+        return res.status(404).render("pages/error", { error: "User not found" });
+      }
+      
+      res.redirect("/users");
+    } catch (error) {
+      console.error("Error updating user:", error);
+      res.status(500).render("pages/error", { 
+        error: "Failed to update user",
+        formData: req.body 
+      });
+    }
+  },
+
+  // Remove the specified user
+  destroy: async (req, res) => {
+    try {
+      const result = await Users.findByIdAndDelete(req.params.userId);
+      
+      if (!result) {
+        return res.status(404).json({
+          success: false,
+          message: "User not found"
+        });
+      }
+      
+      const users = await Users.find().select("name _id");
+      
+      res.json({
+        success: true,
+        data: users,
+        message: "User deleted successfully",
+        method: req.method,
+        url: req.url
+      });
+    } catch (error) {
+      console.error("Error deleting user:", error);
+      res.status(500).json({
+        success: false,
+        message: "Failed to delete user"
+      });
+    }
+  }
 };
